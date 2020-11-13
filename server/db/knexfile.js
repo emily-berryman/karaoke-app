293257742d9debe31ed92156de20b885f4b80530
manucanadas <57819846+manucanadas@users.noreply.github.com>
2020-11-13 15:46:29 +1300

    update db preferences for deploy

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -19,11 +19,12 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
+    // {
+    //   database: 'my_db',
+    //   user: 'username',
+    //   password: 'password'
+    // },
     pool: {
       min: 2,
       max: 10
